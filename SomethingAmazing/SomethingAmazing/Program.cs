f1ae6384eee5e2bb1217a330ffc7d1ffbf6eb9f2
naturallymike <30327729+naturallymike@users.noreply.github.com>
2017-07-20 17:31:33 -0400

    Add files via upload

---
+++ SomethingAmazing/SomethingAmazing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingAmazing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
