b8517ddd7d02686d6b5d885aee5d7ec08483c9b6
naturallymike <30327729+naturallymike@users.noreply.github.com>
2017-07-21 17:56:37 -0400

    sgsf g

--- SomethingAmazing/SomethingAmazing/LocationState/SaveState.cs
+++ SomethingAmazing/SomethingAmazing/LocationState/SaveState.cs
@@ -13,6 +13,38 @@ namespace SomethingAmazing.LocationState
         //class constructor
         public SaveState()
         {
+            // This array is just an attempt at object location state. Each Obj[] is an object. And Obj[] contains a location on the 
+            //map in a xy value. The array should also contain the type of object, such as couch or girl or ball. Obj[] x,y,z and maybe 
+            // z can be the object type.
+            int ObjNum = 100;
+
+            int[] Obj = new int[ObjNum];
+
+            Obj[0]  = 1x1y;
+            Obj[1]  = 1x1y;
+            Obj[2]  = 1x1y;
+            Obj[3]  = 1x1y;
+            Obj[4]  = 1x1y;
+            Obj[5]  = 1x1y;
+            Obj[6]  = 1x1y;
+            Obj[7]  = 1x1y;
+            Obj[8]  = 1x1y;
+            Obj[9]  = 1x1y;
+            Obj[10] = 1x1y;
+            Obj[11] = 1x1y;
+            Obj[12] = 1x1y;
+            Obj[13] = 1x1y;
+            Obj[14] = 1x1y; 
+            Obj[15] = 1x1y;
+            Obj[16] = 1x1y;
+            Obj[17] = 1x1y;
+            Obj[18] = 1x1y;
+            Obj[19] = 1x1y;
+            Obj[20] = 1x1y;
+            Obj[21] = 1x1y;
+            Obj[22] = 1x1y;
+            Obj[23] = 1x1y;
+
         }
 
         //properties
