1af7ffc1ca54094c4a2bf4b4d766e678e2c53bbe
Raster Deluge <rasterDeluge@gmail.com>
2017-07-21 17:00:08 -0400

    sdfg

---
+++ SomethingAmazing/SomethingAmazing/Things/Floor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingAmazing.Things
+{
+    class Floor : Inanimate
+    {
+        //fields or no
+
+        //class constructor
+        public Floor()
+        {
+        }
+
+        //properties
+
+        //methods
+    }
+}
