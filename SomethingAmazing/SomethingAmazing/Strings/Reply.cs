27a8b7e010b2b7d6b1fdc7c28cc51c4a7591dd72
naturallymike <30327729+naturallymike@users.noreply.github.com>
2017-07-24 13:54:38 -0400

    StringCruncher issues

---
+++ SomethingAmazing/SomethingAmazing/Strings/Reply.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SomethingAmazing.Stories;
+
+namespace SomethingAmazing.Strings
+{
+    
+    class Reply
+    {
+
+      
+        int something = 500;
+
+        // This method is for future TalkBots in the game. Each string in the array will have a sorting number, and certain numbers can go
+        //together and some can't
+        public void CharReplies()
+        {
+            
+
+            string[] CharRe = new string[100];
+            
+
+            CharRe[0] = "Hello";
+            CharRe[1] = "Hi";
+            CharRe[2] = "You";
+            CharRe[3] = "Me";
+            CharRe[4] = "?";
+            CharRe[5] = "";
+            CharRe[1] = "";
+            CharRe[2] = "";
+            CharRe[3] = "";
+            CharRe[4] = "";
+            CharRe[5] = ""; 
+            CharRe[2] = "";
+            CharRe[3] = "";
+            CharRe[4] = "";
+            CharRe[5] = "";
+            CharRe[2] = "";
+            CharRe[3] = "";
+            CharRe[4] = "";
+            CharRe[5] = ""; 
+            CharRe[2] = "";
+            CharRe[3] = "";
+            CharRe[4] = "";
+            CharRe[5] = "";
+            
+        }
+        public void Sentences(params string[] talks)
+        {
+            StoryLogic L = new StoryLogic();
+
+            string[] talk = new string[11];
+
+             talks = talk;
+
+            //int Sleep = 3000;
+         
+
+            //L.StringCruncher( talk);
+
+         
+            
+            
+
+
+            talk[0] = "hello";
+            talk[1] = "Yo yo yo";
+            talk[2] = "Okay";
+            talk[3] = "This";
+            talk[4] = "";
+            talk[5] = "";
+            talk[6] = "";
+            talk[7] = "";
+            talk[8] = "";
+            talk[9] = "";
+            talk[10] = "";
+
+      
+
+        }
+    }
+}
